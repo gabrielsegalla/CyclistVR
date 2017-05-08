90b88a81a1875f17a4224e0d7dff6d1e180c4090
gabriel.segalla <gabriel.segalla@sc.senai.br>
2017-05-08 17:36:57 -0300

    Menu funcionando

---
+++ Source/CyclistVR/Carro.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CyclistVR.h"
+#include "Carro.h"
+
+
+// Sets default values
+ACarro::ACarro()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
+	Root->SetCollisionProfileName("OverlapAllDynamic");
+	RootComponent = Root;
+
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionProfileName("BlockAll");
+	MeshComp->SetWorldRotation(FRotator(0.0f, 90.0f, 0.0f));
+	MeshComp->SetWorldScale3D(FVector(1.5f, 1.5f, 1.5f));
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh>
+		Mesh(TEXT("SkeletalMesh'/Game/FBX/carro/carrro3.carrro3'"));
+
+	if (Mesh.Succeeded()) {
+		MeshComp->SetSkeletalMesh(Mesh.Object);
+
+	}
+	MeshComp->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ACarro::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACarro::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//velocidade = Root->GetComponentLocation().X - 1;
+	//Root->SetWorldLocation(velocidade, Root->GetWorldLocation(), z);
+	FVector ActualLocation = Root->GetComponentLocation();
+	ActualLocation.X -= 15;
+	Root->SetWorldLocation(ActualLocation);
+
+}
+
---
+++ Source/CyclistVR/CarsSpawn.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CyclistVR.h"
+#include "CarsSpawn.h"
+#include "Carro.h"
+
+
+// Sets default values
+ACarsSpawn::ACarsSpawn()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("ROOT"));
+	RootComponent = Root;
+	CountCarsToSpawn = 15;
+}
+
+// Called when the game starts or when spawned
+void ACarsSpawn::BeginPlay()
+{
+	Super::BeginPlay();
+	UWorld * World = GetWorld();
+	GetWorld()->GetTimerManager().SetTimer(FuzeTimerHandle, this, &ACarsSpawn::SpawCars, 1.0f, true);
+
+
+
+}
+
+// Called every frame
+void ACarsSpawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACarsSpawn::SpawCars() {
+	UWorld * World = GetWorld();
+	if (World != nullptr) {
+		float LocationX = 5014.0f;
+		float LocationZ = 1182.0f;
+
+
+
+		Ramdom = FMath::RandRange(0, 1);
+		UE_LOG(LogTemp, Warning, TEXT("Random: %d"), Ramdom);
+		FActorSpawnParameters SpawParameters;
+
+		if (Ramdom == 1) {
+			FVector Location(LocationX, 119.0f, LocationZ);
+			ACarro * carro = World->SpawnActor<ACarro>(Location, FRotator::ZeroRotator, SpawParameters);
+			LocationX += 1000.0f;
+			CountCarsToSpawn++;
+			UE_LOG(LogTemp, Warning, TEXT("carro 1"));
+		}
+
+		else if (Ramdom == 0) {
+			FVector Location(LocationX, 235.0f, LocationZ);
+			ACarro * carro = World->SpawnActor<ACarro>(Location, FRotator::ZeroRotator, SpawParameters);
+			LocationX += 1000.0f;
+			CountCarsToSpawn++;
+			UE_LOG(LogTemp, Warning, TEXT("carro 2"));
+		}
+
+
+
+	}
+}
+
